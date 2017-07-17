fed7751322e97ddd90ebcc1476c5d70a28d35868
Grant <grant.lindberg@yahoo.com>
2017-07-17 14:44:07 -0700

    Validate length of message on server side

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import * as ws from "ws";
 
+const MAX_MESSAGE_LENGTH = 300;
+
 type WebSocket = ws & {
   isAlive?: boolean
 };
@@ -15,6 +17,7 @@ server.on("connection", function connection(sock: WebSocket) {
   sock.on("pong", heartbeat);
   sock.on("message", function incoming(data: string) {
     let info = JSON.parse(data);
+    info.message = info.message.substring(0, MAX_MESSAGE_LENGTH);
     server.clients.forEach(function each(client: WebSocket) {
       if(client.readyState === ws.OPEN) {
         client.send(JSON.stringify(info));
